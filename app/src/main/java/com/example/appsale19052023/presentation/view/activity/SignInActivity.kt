7b06498bf5c704daa01142c2639805d95fde3383
phamtanphat <phatdroid94@gmail.com>
2023-10-16 18:08:27 +0700

    ui

--- app/src/main/java/com/example/appsale19052023/presentation/view/activity/SignInActivity.kt
+++ app/src/main/java/com/example/appsale19052023/presentation/view/activity/SignInActivity.kt
@@ -1,17 +1,18 @@
-package com.example.appsale19052023
+package com.example.appsale19052023.presentation.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
+import com.example.appsale19052023.R
 import com.example.appsale19052023.presentation.viewmodel.SignInViewModel
 
-class MainActivity : AppCompatActivity() {
+class SignInActivity : AppCompatActivity() {
 
     private lateinit var signInViewModel: SignInViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_signin)
 
         signInViewModel = ViewModelProvider(this)[SignInViewModel::class.java]
 
