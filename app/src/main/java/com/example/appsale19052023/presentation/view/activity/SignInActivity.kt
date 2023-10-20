bcb251e51d2bda82b2464a28013d40f6b2926660
phamtanphat <phatdroid94@gmail.com>
2023-10-20 18:17:09 +0700

    handle text view register

--- app/src/main/java/com/example/appsale19052023/presentation/view/activity/SignInActivity.kt
+++ app/src/main/java/com/example/appsale19052023/presentation/view/activity/SignInActivity.kt
@@ -1,16 +1,19 @@
 package com.example.appsale19052023.presentation.view.activity
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableStringBuilder
+import android.text.method.LinkMovementMethod
 import android.widget.LinearLayout
 import android.widget.TextView
-import android.widget.Toast
 import androidx.core.view.isGone
-import androidx.core.view.isVisible
 import androidx.lifecycle.ViewModelProvider
 import com.example.appsale19052023.R
 import com.example.appsale19052023.data.api.AppResource
 import com.example.appsale19052023.presentation.viewmodel.SignInViewModel
+import com.example.appsale19052023.util.SpannedUtils
 import com.example.appsale19052023.util.ToastUtils
 import com.google.android.material.textfield.TextInputEditText
 
@@ -58,6 +61,22 @@ class SignInActivity : AppCompatActivity() {
 
             signInViewModel.executeSignIn(email, password)
         }
+
+        displayTextViewRegister()
+    }
+
+    private fun displayTextViewRegister() {
+        SpannableStringBuilder().apply {
+            append("Don't have an account?")
+            append(SpannedUtils.setClickColorLink(
+                text = "Register",
+                context = this@SignInActivity,
+                onListenClick = onClickRegister
+            ))
+            tvRegister.text = this
+            tvRegister.highlightColor = Color.TRANSPARENT
+            tvRegister.movementMethod = LinkMovementMethod.getInstance()
+        }
     }
 
     private fun initView() {
@@ -66,5 +85,9 @@ class SignInActivity : AppCompatActivity() {
         buttonSignIn = findViewById(R.id.button_sign_in)
         tvRegister = findViewById(R.id.text_view_register)
         layoutLoading = findViewById(R.id.layout_loading)
+    }
+
+    private var onClickRegister =  {
+        startActivity(Intent(this@SignInActivity, RegisterActivity::class.java))
     }
 }
