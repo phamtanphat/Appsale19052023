ad523d294913f7912a3e749b2291061675a1012c
phamtanphat <phatdroid94@gmail.com>
2023-10-30 18:10:20 +0700

    share preference

---
+++ app/src/main/java/com/example/appsale19052023/common/AppSharePreference.kt
@@ -0,0 +1,26 @@
+package com.example.appsale19052023.common
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+
+class AppSharePreference(
+    context: Context
+) {
+    private val SHARED_PREFERENCE_NAME = "app-cache"
+
+    private val sharedPreferences: SharedPreferences by lazy {
+        context.getSharedPreferences(SHARED_PREFERENCE_NAME, AppCompatActivity.MODE_PRIVATE)
+    }
+
+    private val editor: SharedPreferences.Editor by lazy {
+        sharedPreferences.edit()
+    }
+
+    fun saveString(key: String, value: String) {
+        editor.putString(key, value)
+        editor.commit()
+    }
+
+    fun getString(key: String) = sharedPreferences.getString(key, "")
+}
